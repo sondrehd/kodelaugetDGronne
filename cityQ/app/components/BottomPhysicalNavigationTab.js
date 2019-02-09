67c019a9adcd83ca59e84416def466fb996a873e
Sondre Dyvik <sondre.dyvik@knowit.no>
2019-02-09 13:35:37 +0100

    Fixed BottomPhysicalNavigation responsiveness

--- cityQ/app/components/BottomPhysicalNavigationTab.js
+++ cityQ/app/components/BottomPhysicalNavigationTab.js
@@ -9,6 +9,7 @@ const BottomPhysicalNavigationTab = props => {
   return (
     <View style={{ position: "absolute", bottom: 40, left: 0, right: 0 }}>
       <BottomNavIcon
+        width='100%'
         style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}
       />
       <View style={[styles.ViewGeneric, { left: 10 }]}>
